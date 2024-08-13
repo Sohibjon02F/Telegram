0d9c4525c987aa5964e4ae4632f272ebc83272ff
Sohibjon <sohibjonx39@gmail.com>
2024-08-13 14:15:25 +0500

    telegramni yukladim

--- tg/lib/group_page.dart
+++ tg/lib/group_page.dart
@@ -5,6 +5,6 @@ class GrouPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("guruhlar"),),);
+    return Scaffold(body: Center(child: Text("guruhfgdlar"),),);
   }
 }
